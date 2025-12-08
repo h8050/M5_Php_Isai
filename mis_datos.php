0caa4e66fe779c6068c3f9df9adbfe77d7fdda8e
Isaí <isaangjim@gmail.com>
2025-12-08 16:35:28 +0000

    Primeros pasos php

---
+++ mis_datos.php
@@ -0,0 +1,10 @@
+    <?php
+    $nombres = "Isaí";
+    $p_apellido = "Angulo";
+    $s_apellido = "Jiménez";
+    $correo = "correo_profesional@dominio.com";
+    $cargo = "Jefe de Área";
+    $domicilio = "CDMX, México";
+
+    $nombre_completo = $nombres . " " . $p_apellido . " " . $s_apellido;
+    ?>
